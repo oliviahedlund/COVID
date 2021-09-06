5daa33170fdee8313dc493076da8d95b7e0bcc15
oliviahedlund <oliviahedlund@live.se>
2021-09-06 10:44:48 +0200

    centralized menu, centralized strings

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,20 +1,30 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends MenuActivity {
+
+    //toolbar menu
+    Toolbar myToolbar;
+    MainActivity mainact = new MainActivity();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        myToolbar = findViewById(R.id.myToolbar); //toolbar menu
+        setSupportActionBar(myToolbar); //toolbar menu
 
         //Buttons for login and register
         Button loginbutton = findViewById(R.id.button1);
@@ -24,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity(DashActivity.class);
+                openActivity(MainActivity.class);
             }
         });
         registerbutton.setOnClickListener(new View.OnClickListener() {
@@ -39,4 +49,5 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, _act);
         startActivity(intent);
     }
+
 }
