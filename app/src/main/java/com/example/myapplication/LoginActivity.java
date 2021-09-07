1f1008eb7c0d0c6fb15d2e38cfb27d1ed4cddb9c
linda <lindamcc98@live.com>
2021-09-07 14:06:33 +0200

    kollade om lösen och mail är korrekt, isf visas dashboard men inte annars.

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -14,17 +16,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        //Edit Text for user email and password
+        EditText userEmail = findViewById(R.id.editTextTextEmailAddress3);
+        EditText userPassword = findViewById(R.id.editTextTextPassword2);
 
         //Buttons for login and register
         Button loginbutton = findViewById(R.id.button1);
         TextView registerbutton = findViewById(R.id.textView5);
 
+
         //open new activity when clicked
         loginbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity(DashActivity.class);
+                validate(userEmail.getText().toString(), userPassword.getText().toString());
             }
         });
         registerbutton.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +38,17 @@ public class LoginActivity extends AppCompatActivity {
                 openActivity(RegisterActivity.class);
             }
         });
+    }
+
+    public void validate(String userEmail, String userPassword){
+        if((userEmail.equals("test@gmail.com")) && (userPassword.equals("123"))){
+            openActivity(DashActivity.class);
+        }
+        else{
+            TextView incorrectLogin = findViewById(R.id.textView7);
+            incorrectLogin.setText("Incorrect email or password");
+        }
+
     }
 
     public void openActivity(Class _act){
