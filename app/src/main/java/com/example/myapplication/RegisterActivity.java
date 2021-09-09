3d82bb4c8673cb91c0cb85d0a7280e6b1acdaa58
linda <lindamcc98@live.com>
2021-09-09 15:03:34 +0200

    email check

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -5,11 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterActivity extends MenuActivity {
 
@@ -23,15 +29,35 @@ public class RegisterActivity extends MenuActivity {
         myToolbar = findViewById(R.id.myToolbar); //toolbar menu
         setSupportActionBar(myToolbar); //toolbar menu
 
+
+
+        EditText userEmail = findViewById(R.id.editTextTextEmailAddress3);
+
         Button registerbutton = findViewById(R.id.button2);
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openActivity(MainActivity.class);
+                validate_registration(userEmail.getText().toString());
             }
         });
 
     }
+
+    public void validate_registration(String userEmail){
+        TextView email = findViewById(R.id.textView3);
+
+        Pattern pattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
+        Matcher mat = pattern.matcher(userEmail);
+        if(mat.matches()){
+            System.out.println("Valid email address");
+            openActivity(MainActivity.class);
+        }else{
+            System.out.println("Not a valid email address");
+            email.setTextColor(Color.parseColor("#ff0000"));
+        }
+
+    }
+
     public void openActivity(Class _act){
         Intent intent = new Intent(this, _act);
         startActivity(intent);
