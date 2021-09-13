37d7b6dcf7619b1fd1531479e3a5461f8b2396af
Jesper <jesperlindh19@gmail.com>
2021-09-13 15:54:57 +0200

    Fixes in RegisterActivity

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,16 +1,20 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegisterActivity extends AppCompatActivity {
+    final int AGE_REQUIREMENT = 18000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +26,14 @@ public class RegisterActivity extends AppCompatActivity {
         registerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(validate_registration()){
-                    openActivity(MainActivity.class);
-                }
-                else{
-                    System.out.println("onClick error");
-                }
+                if(validate_registration()) openActivity(MainActivity.class);
             }
         });
     }
 
     private boolean validate(TextView text, EditText input, Pattern pattern, String id){
-        Matcher mat = pattern.matcher(input.toString());
+        Matcher mat = pattern.matcher(input.getText().toString());
+        System.out.println("Input: " + input);
         if(mat.matches()){
             text.setTextColor(getResources().getColor(R.color.black));
             return true;
@@ -53,10 +53,33 @@ public class RegisterActivity extends AppCompatActivity {
         }
     }
 
+    private boolean repeat_password(String repeatPassword, String password, TextView text){
+        if(repeatPassword.equals(password)){
+            text.setTextColor(getResources().getColor(R.color.black));
+            return true;
+        }
+        else{
+            System.out.println("Passwords do not match");
+            text.setTextColor(getResources().getColor(R.color.red));
+            return false;
+        }
+    }
+
+    private boolean validate_date(){
+        if(true){
+            System.out.println(System.currentTimeMillis());
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
     public boolean validate_registration(){
         TextView name = findViewById(R.id.textView2);
         TextView email = findViewById(R.id.textView3);
         TextView password = findViewById(R.id.textView4);
+        TextView repeatPassword = findViewById(R.id.textView6);
         TextView phone = findViewById(R.id.textView8);
         TextView date = findViewById(R.id.textView9);
         TextView street = findViewById(R.id.textView10);
@@ -67,7 +90,7 @@ public class RegisterActivity extends AppCompatActivity {
         EditText userName = findViewById(R.id.editTextTextPersonName3);
         EditText userEmail = findViewById(R.id.editTextTextEmailAddress3);
         EditText userPassword = findViewById(R.id.editTextTextPassword);
-        //Repeat password?
+        EditText userRepeatPassword = findViewById(R.id.editTextTextPassword2);
         EditText userPhone = findViewById(R.id.editTextPhone);
         EditText userDate = findViewById(R.id.editTextDate);
         EditText userStreet = findViewById(R.id.editTextTextPostalAddress);
@@ -75,35 +98,31 @@ public class RegisterActivity extends AppCompatActivity {
         EditText userCity = findViewById(R.id.editTextTextPersonName);
         EditText userCounty = findViewById(R.id.editTextTextPersonName2);
 
-        Pattern namePattern = Pattern.compile/*("[A-Za-z_]*");*/("Jesper");
+        Pattern namePattern = Pattern.compile("[A-Za-z_ ]{1,30}");
         Pattern emailPattern = Pattern.compile("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}");
-        Pattern passwordPattern = Pattern.compile("[\\x00-\\x7F]{5,20}"); //all ascii - 5-20 symbols
-        Pattern phonePattern = Pattern.compile("[+]{0,1}+[-][0,1]+[0-9]{9,12}");
-        Pattern datePattern = Pattern.compile("[0-9]{6,8}");
-        Pattern streetPattern = Pattern.compile /*("\s*[\p{L}][\p{L}_0-9]*\s");*/ ("[A-Za-z0-9_]{4,30}"); //åäö?
+        Pattern passwordPattern = Pattern.compile("[\\x00-\\x7F]{8,20}"); //all ascii - 8-20 symbols - stor bokstav & siffra
+        Pattern phonePattern = Pattern.compile("[0-9]{10,11}");
+        Pattern datePattern = Pattern.compile("[0-9]{8}");
+        Pattern streetPattern = Pattern.compile("[A-Za-z0-9_ ]{4,30}");
         Pattern zipPattern = Pattern.compile("[0-9]{5}");
-        Pattern cityPattern = Pattern.compile("[A-Za-z]{2,15}");
-        Pattern countyPattern = Pattern.compile("[A-Za-z]*"); //åäö?
-
+        Pattern cityCountyPattern = Pattern.compile("[A-Za-z]{2,15}");
 
         if(validate(name, userName, namePattern, "userName") &&
                 validate(email, userEmail, emailPattern, "email") &&
-                validate(password, userPassword, passwordPattern, "UserPassword") &&
+                validate(password, userPassword, passwordPattern, "userPassword") &&
+                repeat_password(userRepeatPassword.getText().toString(), userPassword.getText().toString(), repeatPassword) &&
                 validate(phone, userPhone, phonePattern, "userPhone") &&
                 validate(date, userDate, datePattern, "userDate") &&
                 validate(street, userStreet, streetPattern, "userStreet") &&
                 validate(zip, userZip, zipPattern, "userZip") &&
-                validate(city, userCity, cityPattern, "userCity") &&
-                validate(county, userCounty, countyPattern, "userCounty")
+                validate(city, userCity, cityCountyPattern, "userCity") &&
+                validate(county, userCounty, cityCountyPattern, "userCounty")
 
         ){
             openActivity(MainActivity.class);
             return true;
         }
-        else{
-            System.out.println("validate error");
-            return false;
-        }
+        return false;
     }
 
     public void openActivity(Class _act){
