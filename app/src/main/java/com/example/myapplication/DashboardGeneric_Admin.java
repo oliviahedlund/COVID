dcfa2e6eaff7dbb78ecea49bc9676acff379fac6
Jesper <jesperlindh19@gmail.com>
2021-10-05 18:14:53 +0200

    .

--- app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
+++ app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
@@ -22,10 +22,10 @@ public class DashboardGeneric_Admin extends Fragment {
     private String[] ageGroup;
     private String[] product;
     private String[] dose;
-    private AutoCompleteTextView locationFilter;
-    private AutoCompleteTextView ageGroupFilter;
-    private AutoCompleteTextView productFilter;
-    private AutoCompleteTextView doseFilter;
+    private AutoCompleteTextView countyFilter; //Prev countyFilter
+    private AutoCompleteTextView centerFilter; //Prev centerFilter
+    private AutoCompleteTextView dateFilter; //Prev dateFilter
+    //private AutoCompleteTextView doseFilter;
 
     private ArrayList<Covid_Tracking_ListViewCell> listViewItems = new ArrayList<Covid_Tracking_ListViewCell>();
     private ListView listView;
@@ -51,32 +51,33 @@ public class DashboardGeneric_Admin extends Fragment {
         // Inflate the layout for this fragment
         return view;
     }
-    private void setupDropdownMenus(){
+
+    private void setupDropdownMenus() {
         //Filter
         locations = getResources().getStringArray(R.array.location);
         ageGroup = getResources().getStringArray(R.array.ageGroup);
         product = getResources().getStringArray(R.array.product);
         dose = getResources().getStringArray(R.array.dose);
 
-        locationFilter = (AutoCompleteTextView) view.findViewById(R.id.locationFilter);
-        ageGroupFilter = (AutoCompleteTextView) view.findViewById(R.id.ageGroupFilter);
-        productFilter = (AutoCompleteTextView) view.findViewById(R.id.productFilter);
-        doseFilter = (AutoCompleteTextView) view.findViewById(R.id.doseFilter);
+        countyFilter = (AutoCompleteTextView) view.findViewById(R.id.countyFilter);
+        centerFilter = (AutoCompleteTextView) view.findViewById(R.id.centerFilter);
+        dateFilter = (AutoCompleteTextView) view.findViewById(R.id.dateFilter);
+        //doseFilter = (AutoCompleteTextView) view.findViewById(R.id.doseFilter);
 
         Covid_Tracking_DropdownAdapter locationAdapter = new Covid_Tracking_DropdownAdapter(this.getContext(), R.layout.covid_tracking_dropdown_item, locations);
 
-        locationFilter.setAdapter(locationAdapter);
+        countyFilter.setAdapter(locationAdapter);
 
         Covid_Tracking_DropdownAdapter ageGroupAdapter = new Covid_Tracking_DropdownAdapter(this.getActivity().getApplicationContext(), R.layout.covid_tracking_dropdown_item, ageGroup);
-        ageGroupFilter.setAdapter(ageGroupAdapter);
+        centerFilter.setAdapter(ageGroupAdapter);
         Covid_Tracking_DropdownAdapter productAdapter = new Covid_Tracking_DropdownAdapter(this.getActivity().getApplicationContext(), R.layout.covid_tracking_dropdown_item, product);
-        productFilter.setAdapter(productAdapter);
+        dateFilter.setAdapter(productAdapter);
         Covid_Tracking_DropdownAdapter doseAdapter = new Covid_Tracking_DropdownAdapter(this.getActivity().getApplicationContext(), R.layout.covid_tracking_dropdown_item, dose);
-        doseFilter.setAdapter(doseAdapter);
+        //doseFilter.setAdapter(doseAdapter);
 
     }
 
-    private void setupListViewItems(){
+    private void setupListViewItems() {
 //        OBS2: How to display the data according to the filter. when we have gotten the data from the database, we can set each ListViewCell data section in order to display the data.
 //        we can use for loop to cycle through the listViewItems, put the listViewCells we need to display in the newly create ArryaList
 //        and use adapter to display them
@@ -106,7 +107,7 @@ public class DashboardGeneric_Admin extends Fragment {
     }
 
 
-    private void setupFilter(){
+    private void setupFilter() {
         filterButton = (Button) view.findViewById(R.id.filterButton);
         filterTabs1 = (LinearLayout) view.findViewById(R.id.filterTabs1);
         filterTabs2 = (LinearLayout) view.findViewById(R.id.filterTabs2);
@@ -114,57 +115,64 @@ public class DashboardGeneric_Admin extends Fragment {
         filterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(filterHidden == true){
+                if (filterHidden == true) {
                     filterHidden = false;
-                    showFilter();
-                } else{
+                    filterTabs1.setVisibility(View.VISIBLE);
+                    filterTabs2.setVisibility(View.VISIBLE);
+                    filterButton.setText("HIDE");
+                    //showFilter();
+                } else {
                     filterHidden = true;
-                    hideFilter();
+                    filterTabs1.setVisibility(View.GONE);
+                    filterTabs2.setVisibility(View.GONE);
+                    filterButton.setText("FILTER");
+                    //hideFilter();
                 }
             }
         });
 // OBS1: use these filter to build a e.g. SQL query and query the API or database and then get the data and parse the data and display the data in the list view.
-        locationFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        countyFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                Log.d("selected", String.valueOf(locationFilter.getText()));
+                Log.d("selected", String.valueOf(countyFilter.getText()));
                 Log.d("int", String.valueOf(position));
                 ArrayList<Covid_Tracking_ListViewCell> updatedListView = new ArrayList<Covid_Tracking_ListViewCell>();
 
             }
         });
 
-        ageGroupFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        centerFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
             }
         });
 
-        productFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        dateFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
             }
         });
 
-        doseFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*doseFilter.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
             }
         });
-    }
+    }*/
 
-    private void hideFilter() {
-        filterTabs1.setVisibility(View.GONE);
-        filterTabs2.setVisibility(View.GONE);
-        filterButton.setText("FILTER");
-    }
+        /*private void hideFilter(){
+            filterTabs1.setVisibility(View.GONE);
+            filterTabs2.setVisibility(View.GONE);
+            filterButton.setText("FILTER");
+        }
 
-    private void showFilter(){
-        filterTabs1.setVisibility(View.VISIBLE);
-        filterTabs2.setVisibility(View.VISIBLE);
-        filterButton.setText("HIDE");
+        private void showFilter(){
+            filterTabs1.setVisibility(View.VISIBLE);
+            filterTabs2.setVisibility(View.VISIBLE);
+            filterButton.setText("HIDE");
+        }*/
     }
 }
