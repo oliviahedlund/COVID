ecea0b7629e4f9c1b86ba53eb50d112d83490cdc
Jesper <jesperlindh19@gmail.com>
2021-10-06 15:14:18 +0200

    Filter till län, center & månad fixad

--- app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
+++ app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
@@ -30,16 +30,15 @@ import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-
-
 public class DashboardGeneric_Admin extends Fragment {
-    private String[] locations;
-    private String[] ageGroup;
-    private String[] product;
-    private String[] CountyItems = new String[]{"County","värmland","bohuslän","malmö"};
-    private String[] CenterItems = new String[]{"Center"};
-    private String[] DateItems = new String[]{"Date","okt","nov","dec"};
-
+    //private String[] locations;
+    //private String[] ageGroup;
+    //private String[] product;
+    private String[] CountyItems = new String[]{"Choose county","Blekinge", "Dalarna", "Gotland", "Gävleborg", "Halland",
+            "Jämtland", "Jönköping", "Kalmar", "Kronoberg", "Norrbotten", "Skåne", "Stockholm", "Södermanland", "Uppsala",
+            "Värmland", "Västerbotten", "Västernorrland", "Västmanland", "Västra Götaland", "Örebro", "Östergötland"};
+    private String[] CenterItems = new String[]{"Choose center", "Center 1", "Center 2"};
+    private String[] MonthItems = new String[]{"Choose month","Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Okt","Nov","Dec"};
 
     View view;
 
@@ -48,9 +47,7 @@ public class DashboardGeneric_Admin extends Fragment {
                              Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.activity_admin_dashboard_generic, container, false);
-
         setupDropdownMenus();
-
 
         // Inflate the layout for this fragment
         return view;
@@ -61,17 +58,46 @@ public class DashboardGeneric_Admin extends Fragment {
         Spinner County_dropdown = (Spinner) view.findViewById(R.id.spinner1);
         Spinner Center_dropdown = (Spinner) view.findViewById(R.id.spinner2);
         Spinner Date_dropdown = (Spinner) view.findViewById(R.id.spinner3);
-        //show string array on spinners
+
+        //Show string array on spinners
         ArrayAdapter<String> adapterCounty = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, CountyItems);
-        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, CenterItems);
-        ArrayAdapter<String> adapterDate = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, DateItems);
-        //set arrayadapters on spinner
+        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, /*getCenters(County_dropdown.getSelectedItem().toString())*/ CenterItems);
+        ArrayAdapter<String> adapterDate = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, MonthItems);
+
+        //Set array adapter on spinners
         County_dropdown.setAdapter(adapterCounty);
         Center_dropdown.setAdapter(adapterCenter);
         Date_dropdown.setAdapter(adapterDate);
-
     }
 
+    private String[] getCenters(String county){
+        switch(county){
+            case "Blekinge":
+            case "Dalarna":
+            case "Gotland":
+            case "Gävleborg":
+            case "Halland":
+            case "Jämtland":
+            case "Jönköping":
+            case "Kalmar":
+            case "Kronoberg":
+            case "Norrbotten":
+            case "Skåne":
+            case "Stockholm":
+            case "Södermansland":
+            case "Uppsala":
+            case "Värmland":
+            case "Västerbotten":
+            case "Västernorrland":
+            case "Västmanland":
+            case "Västra Götaland":
+            case "Örebro":
+            case "Östergötland":
+                return new String[]{"Center", "Center 1", "Center 2"};
+            default:
+                return new String[]{"Center"};
+        }
+    }
 
 }
 
