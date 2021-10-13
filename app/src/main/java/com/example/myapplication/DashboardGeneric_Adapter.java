205a82dc41081183be2018f7346b7ab930b99960
kolaper <forsberg.albin@telia.com>
2021-10-13 13:10:08 +0200

    .

--- app/src/main/java/com/example/myapplication/DashboardGeneric_Adapter.java
+++ app/src/main/java/com/example/myapplication/DashboardGeneric_Adapter.java
@@ -17,21 +17,22 @@ public class DashboardGeneric_Adapter extends ArrayAdapter<DashboardGeneric_Cell
 
     public DashboardGeneric_Adapter(@NonNull Context context, int resource, @NonNull ArrayList<DashboardGeneric_Cell> objects) {
         super(context, resource, objects);
+
     }
+
+
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         String Apptime = getItem(position).getAppTime();
-
-        DashboardGeneric_Cell appointment = new DashboardGeneric_Cell(Apptime);
+        DashboardGeneric_Cell appointment = new DashboardGeneric_Cell( Apptime);
 
         if(convertView == null){
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.dashboard_generic_adapter_view, parent, false);
         }
 
         TextView tvApptime = (TextView) convertView.findViewById(R.id.Textview1);
-
         tvApptime.setText(Apptime);
         return convertView;
     }
--- app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
+++ app/src/main/java/com/example/myapplication/DashboardGeneric_Admin.java
@@ -59,12 +59,174 @@ public class DashboardGeneric_Admin extends Fragment {
         activity = (AdminActivity) getActivity();
         user = activity.getUserData();
         getAppointmentApi();
+        /*
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) { //pauses the thread for 10 sec, to get api call -- funka fan inte
+            e.printStackTrace();
+        }
+         */
         setupDropdownMenus();
-        setupListView();
+       // setupListView();
 
         // Inflate the layout for this fragment
         return view;
     }
+
+
+
+    private void setupDropdownMenus(){ // sets up the dropdown menues
+        //Setup spinners
+        Spinner County_dropdown = (Spinner) view.findViewById(R.id.spinner1);
+        Spinner Center_dropdown = (Spinner) view.findViewById(R.id.spinner2);
+        Spinner Date_dropdown = (Spinner) view.findViewById(R.id.spinner3);
+
+        //Show string array on spinners
+        ArrayAdapter<String> adapterCounty = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, CountyItems);
+        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, /*getCenters(County_dropdown.getSelectedItem().toString())*/ CenterItems);
+        ArrayAdapter<String> adapterDate = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, MonthItems);
+
+        //Set array adapter on spinners
+        County_dropdown.setAdapter(adapterCounty);
+        Center_dropdown.setAdapter(adapterCenter);
+        Date_dropdown.setAdapter(adapterDate);
+
+        County_dropdown.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) { // här ska funktion kallas på så centers sorteras och blir displayade beroende på county
+
+
+                        Object CountyItem = parent.getItemAtPosition(pos);
+                        System.out.println(CountyItem.toString());     //prints the text in spinner item.
+                        getCenters(CountyItem.toString(),Center_dropdown);
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+        Center_dropdown.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) { // här ska funktion kallas på så att appointments sorteras och displayas beroende på center
+
+                        Object CenterItem = parent.getItemAtPosition(pos);
+                        System.out.println(CenterItem.toString());     //prints the text in spinner item.
+
+                    }
+                    public void onNothingSelected(AdapterView<?> parent) {
+                    }
+                });
+    }
+    private void getCenters(String county,Spinner Center_dropdown){
+
+        if(county.equals("Choose county")){
+            System.out.println("Default choice (choose county)");
+            CenterItems = new String[]{"Choose center"};
+            ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item,CenterItems);
+            Center_dropdown.setAdapter(adapterCenter);
+            return;
+        }
+        String[] Centers = new String[]{"Choose citron"};// sätt till enbart {}
+        int i=0,j=0;
+        /*
+        while(list_of_centerIDs != NULL){ //sorts the centers depending on county
+            if(list_of_centerIDs[i].getCounty().equals(county)){
+                Centers[j] = list_of_centerIDs[i].getCounty();
+                j++;
+            }
+            i++;
+        }
+         */
+
+        setCenter(Centers,Center_dropdown);
+        // String[] Centers = new String[]{"Choose center"};
+        /*
+        switch(county){
+            case "Blekinge":
+            case "Dalarna":
+            case "Gotland":
+            case "Gävleborg":
+            case "Halland":
+            case "Jämtland":
+            case "Jönköping":
+            case "Kalmar":
+            case "Kronoberg":
+            case "Norrbotten":
+            case "Skåne":
+            case "Stockholm":
+            case "Södermansland":
+            case "Uppsala":
+            case "Värmland":
+            case "Västerbotten":
+            case "Västernorrland":
+            case "Västmanland":
+            case "Västra Götaland":
+            case "Örebro":
+            case "Östergötland":
+                return new String[]{"Center", "Center 1", "Center 2"};
+            default:
+                return new String[]{"Center"};
+        }
+
+         */
+
+
+    }// end of getCenter
+
+    private void setCenter(String[] Centers,Spinner Center_dropdown){
+        System.out.println("Center Set");
+     //   CenterItems = new String[]{"Choose citron"};
+        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item,Centers);
+        //ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item,CenterItems);
+        Center_dropdown.setAdapter(adapterCenter);
+
+    }
+
+    /*private void getUserInfoApi(){
+        Call<UserInfo> userInfoCall = ApiClient.getUserService().getUserInfoAll(user.getToken(), appointmentResponse.get(0).getUserId());
+    }*/
+
+
+
+    private void getAppointmentApi(){
+        Call<List<AppointmentResponse>> appointmentResponseCall = ApiClient.getUserService().getAllAppointments(user.getToken(), false);
+        appointmentResponseCall.enqueue(new Callback<List<AppointmentResponse>>() {
+
+            @RequiresApi(api = Build.VERSION_CODES.O) // OLD - Delete
+            @Override
+            public void onResponse(Call<List<AppointmentResponse>> call, Response<List<AppointmentResponse>> response) {
+
+                //errorhandling
+                if (response.isSuccessful()) {
+                    appointmentResponse = response.body(); //i userResponse ligger all information om användaren
+
+                    //appointmentResponse.get(10).getTime(); //Returnerar Id:t på 11e tiden
+                    if(appointmentResponse.size() > 0) {
+                        System.out.println("Size: " + appointmentResponse.size());
+                        System.out.println("ID: " + appointmentResponse.get(0).getId());
+                        System.out.println("User ID: " + appointmentResponse.get(0).getUserId());
+                        System.out.println("Center ID: " + appointmentResponse.get(0).getCenterId());
+                        System.out.println("Vaccine ID: " + appointmentResponse.get(0).getVaccineId());
+                        System.out.println("Time: " + appointmentResponse.get(0).getTime());
+                        System.out.println("Length: " + appointmentResponse.get(0).getLength());
+                        setupListView();
+                    }
+                    else{
+                        System.out.println("Empty appointmentResponse");
+                    }
+                }else{
+                    Toast.makeText(activity,"Appointments error", Toast.LENGTH_LONG).show();
+                    System.out.println("Fail - else");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<AppointmentResponse>> call, Throwable t) {
+                Toast.makeText(activity,"Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                System.out.println("Fail - onFailure: " + t.getLocalizedMessage());
+            }
+        });
+
+    }//end of getAppointment
+
 
     private void setupListView() { // sets up list view with booked appointments depending on the filter
         appointments = (ListView) view.findViewById(R.id.list);
@@ -72,14 +234,18 @@ public class DashboardGeneric_Admin extends Fragment {
 
         //makes an arraylist of custom datatype
         ArrayList<DashboardGeneric_Cell> app_list  = new ArrayList<DashboardGeneric_Cell>();
-        DashboardGeneric_Cell[] cells = new DashboardGeneric_Cell[appointmentResponse.size()];
+        DashboardGeneric_Cell[] cells = new DashboardGeneric_Cell[appointmentResponse.size()];//appointmentResponse.size()
 
-        //creates objects
-        //adds every object to the arraylist
-        for(int i =0; i<appointmentResponse.size(); i++){
-          cells[i] = new DashboardGeneric_Cell(appointmentResponse.get(i).getTime());
-          app_list.add(cells[i]);
+      //  System.out.println("Size: " + appointmentResponse.size());
+
+        //for loop that adds times from api call to a list
+        for(int i =0; i<appointmentResponse.size(); i++){ //appointmentResponse.size()
+            cells[i] = new DashboardGeneric_Cell(appointmentResponse.get(i).getTime()); //appointmentResponse.get(i).getTime()
+            System.out.println("cells :"+ cells[i]);
+            app_list.add(cells[i]);
         }
+       // DashboardGeneric_Cell matt0 = new DashboardGeneric_Cell("12:00");
+        //app_list.add(matt0);
 /*
         //creates objects
         appointments = (ListView) view.findViewById(R.id.list) ;
@@ -116,141 +282,6 @@ public class DashboardGeneric_Admin extends Fragment {
         //creates and sets custom adapter to the listview
         DashboardGeneric_Adapter AppAdapter = new DashboardGeneric_Adapter(this.getContext(), 0, app_list);
         appointments.setAdapter(AppAdapter);
-    }
+    }// end of setUpListView
 
-    private void setupDropdownMenus(){ // sets up the dropdown menues
-        //Setup spinners
-        Spinner County_dropdown = (Spinner) view.findViewById(R.id.spinner1);
-        Spinner Center_dropdown = (Spinner) view.findViewById(R.id.spinner2);
-        Spinner Date_dropdown = (Spinner) view.findViewById(R.id.spinner3);
-
-        //Show string array on spinners
-        ArrayAdapter<String> adapterCounty = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, CountyItems);
-        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, /*getCenters(County_dropdown.getSelectedItem().toString())*/ CenterItems);
-        ArrayAdapter<String> adapterDate = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item, MonthItems);
-
-        //Set array adapter on spinners
-        County_dropdown.setAdapter(adapterCounty);
-        Center_dropdown.setAdapter(adapterCenter);
-        Date_dropdown.setAdapter(adapterDate);
-
-        County_dropdown.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) { // här ska funktion kallas på så centers sorteras och blir displayade beroende på county
-
-                        Object CountyItem = parent.getItemAtPosition(pos);
-                        System.out.println(CountyItem.toString());     //prints the text in spinner item.
-                        getCenters(CountyItem.toString(),Center_dropdown);
-                    }
-                    public void onNothingSelected(AdapterView<?> parent) {
-                    }
-                });
-        Center_dropdown.setOnItemSelectedListener(
-                new AdapterView.OnItemSelectedListener() {
-                    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) { // här ska funktion kallas på så att appointments sorteras och displayas beroende på center
-
-                        Object CenterItem = parent.getItemAtPosition(pos);
-                        System.out.println(CenterItem.toString());     //prints the text in spinner item.
-
-                    }
-                    public void onNothingSelected(AdapterView<?> parent) {
-                    }
-                });
-    }
-    private void getCenters(String county,Spinner Center_dropdown){
-
-        if(county.equals("Choose county")){
-            System.out.println("Default choice (choose county)");
-            CenterItems = new String[]{"Choose center"};
-            ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item,CenterItems);
-            Center_dropdown.setAdapter(adapterCenter);
-            return;
-        }
-       // String[] Centers = new String[]{"Choose center"};
-        /*
-        switch(county){
-            case "Blekinge":
-            case "Dalarna":
-            case "Gotland":
-            case "Gävleborg":
-            case "Halland":
-            case "Jämtland":
-            case "Jönköping":
-            case "Kalmar":
-            case "Kronoberg":
-            case "Norrbotten":
-            case "Skåne":
-            case "Stockholm":
-            case "Södermansland":
-            case "Uppsala":
-            case "Värmland":
-            case "Västerbotten":
-            case "Västernorrland":
-            case "Västmanland":
-            case "Västra Götaland":
-            case "Örebro":
-            case "Östergötland":
-                return new String[]{"Center", "Center 1", "Center 2"};
-            default:
-                return new String[]{"Center"};
-        }
-
-         */
-
-        CenterItems = new String[]{"Choose citron"};
-        ArrayAdapter<String> adapterCenter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_dropdown_item,CenterItems);
-        Center_dropdown.setAdapter(adapterCenter);
-        //setCenter(Centers,Center_dropdown);
-    }// end of getCenter
-
-    private void setCenter(String[] Centers,Spinner Center_dropdown){
-        System.out.println("Center Set");
-
-
-    }
-
-    /*private void getUserInfoApi(){
-        Call<UserInfo> userInfoCall = ApiClient.getUserService().getUserInfoAll(user.getToken(), appointmentResponse.get(0).getUserId());
-    }*/
-
-    private void getAppointmentApi(){
-        Call<List<AppointmentResponse>> appointmentResponseCall = ApiClient.getUserService().getAllAppointments(user.getToken(), false);
-        appointmentResponseCall.enqueue(new Callback<List<AppointmentResponse>>() {
-
-            @RequiresApi(api = Build.VERSION_CODES.O) // OLD - Delete
-            @Override
-            public void onResponse(Call<List<AppointmentResponse>> call, Response<List<AppointmentResponse>> response) {
-
-                //errorhandling
-                if (response.isSuccessful()) {
-                    appointmentResponse = response.body(); //i userResponse ligger all information om användaren
-
-                    //appointmentResponse.get(10).getTime(); //Returnerar Id:t på 11e tiden
-                    if(appointmentResponse.size() > 0) {
-                        System.out.println("ID: " + appointmentResponse.get(0).getId());
-                        System.out.println("User ID: " + appointmentResponse.get(0).getUserId());
-                        System.out.println("Center ID: " + appointmentResponse.get(0).getCenterId());
-                        System.out.println("Vaccine ID: " + appointmentResponse.get(0).getVaccineId());
-                        System.out.println("Time: " + appointmentResponse.get(0).getTime());
-                        System.out.println("Length: " + appointmentResponse.get(0).getLength());
-                    }
-                    else{
-                        System.out.println("Empty appointmentResponse");
-                    }
-                }else{
-                    Toast.makeText(activity,"Appointments error", Toast.LENGTH_LONG).show();
-                    System.out.println("Fail - else");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<AppointmentResponse>> call, Throwable t) {
-                Toast.makeText(activity,"Throwable " + t.getLocalizedMessage(), Toast.LENGTH_LONG).show();
-                System.out.println("Fail - onFailure: " + t.getLocalizedMessage());
-            }
-        });
-
-    }//end of getAppointment
-}
-
-
+}//end of class
