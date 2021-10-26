b4ae6e9789fc7a47dfc81a93d40d57828a0a4f4d
Baiwei <byronquan@gmail.com>
2021-10-26 09:22:40 +0200

    Added newly created covid tracking dashboard to master branch

--- app/src/main/java/com/example/myapplication/AdminActivity.java
+++ app/src/main/java/com/example/myapplication/AdminActivity.java
@@ -20,7 +20,6 @@ import com.example.myapplication.Helpers.CenterVaccineHelper;
 import com.example.myapplication.UI.AdminBooking.AdminAddCenterFragment;
 import com.example.myapplication.UI.AdminBooking.AdminAddVaccineFragment;
 import com.example.myapplication.UI.AdminBooking.AdminBookingRangeFragment;
-import com.example.myapplication.UI.Covid_tracking.Covid_Tracking_tab_cases;
 import com.example.myapplication.UI.Covid_tracking.Covid_Tracking_tabs_fragment;
 import com.example.myapplication.UI.LoadingAnimation;
 import com.example.myapplication.UI.Settings.SettingsFragment;
